686a2f759b3f11b5a88ab515c5ad74ed1fb92e40
paugr <paugr@yahoo.es>
2019-02-13 18:49:52 +0100

    Practice 1

--- P1 Folder/Seq.py
+++ P1 Folder/Seq.py
@@ -1,4 +1,4 @@
-print("Practice 1")
+
 
 # Creation of seq class
 class Seq:
@@ -10,14 +10,45 @@ class Seq:
         return len(self.strbases)
 
     def complement(self):
-        pass
-        #comp_str=[]
-        # pattern = {'A': 'T', 'C': 'G', 'G': 'C', 'T': 'A'}
-        #for base in string:
-        #   comp_str.append(pattern[base])
-    # here object complement()
+        comp_str=""
+        pattern = {'A': 'T', 'C': 'G', 'G': 'C', 'T': 'A'}
+        for base in self.strbases:
+            comp_str+=pattern[base]
+        return comp_str
 
     def reverse(self):
-        return reversed(self.strbases)
+        rev_str = self.strbases[::-1]
+        return rev_str
 
-    #count(base) and perc(base) need base but also self
+    def count(base):
+        A = 0
+        C = 0
+        T = 0
+        G = 0
+        for index in info:
+            if len(index) != 0 and index[0] != '>':
+                A += index.count('A')
+                C += index.count('C')
+                T += index.count('T')
+                G += index.count('G')
+        return A, C, T, G
+
+    def perc(base):
+        tl = len(element)
+        n = s1.count(base)#finish this
+        if tl > 0:
+            perca = round(100.0 * na / tl, 1)
+            percc = round(100.0 * nc / tl, 1)
+            perct = round(100.0 * nt / tl, 1)
+            percg = round(100.0 * ng / tl, 1)
+        else:
+            perca = 0
+            percc = 0
+            perct = 0
+            percg = 0
+
+print("Practice 1")
+
+s1=Seq("AATG")
+s1.complement()
+s1.reverse()
