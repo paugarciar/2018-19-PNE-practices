59dacbdea33fd7bbc2856b4b1b9c961009f560ca
paugarciar <paugr@yahoo.es>
2019-02-17 01:11:34 +0100

    practice 1

--- P1 Folder/Seq.py
+++ P1 Folder/Seq.py
@@ -1,54 +1,35 @@
-
-
 # Creation of seq class
+
 class Seq:
     """A class for representing sequences"""
+
     def __init__(self, strbases):
-        self.strbases = strbases
+        self.strbases = strbases  # self.strbases now represents my initial string
 
     def len(self):
-        return len(self.strbases)
+        return len(self.strbases)  # returns the length of our string(self.strbases)
 
     def complement(self):
-        comp_str=""
+        comp_str = ""  # this empty string will receive the complementary bases
         pattern = {'A': 'T', 'C': 'G', 'G': 'C', 'T': 'A'}
         for base in self.strbases:
-            comp_str+=pattern[base]
+            comp_str += pattern[base]  # complementary bases are searched in the dictionary
         return comp_str
 
     def reverse(self):
-        rev_str = self.strbases[::-1]
+        rev_str = self.strbases[::-1]  # inverting the order of the bases
         return rev_str
 
-    def count(base):
-        A = 0
-        C = 0
-        T = 0
-        G = 0
-        for index in info:
-            if len(index) != 0 and index[0] != '>':
-                A += index.count('A')
-                C += index.count('C')
-                T += index.count('T')
-                G += index.count('G')
-        return A, C, T, G
+    def count(self, base):
+        result = self.strbases.count(base)  # counting the base that we will introduce
+        return result
 
-    def perc(base):
-        tl = len(element)
-        n = s1.count(base)#finish this
+    def perc(self, base):
+        tl = len(self.strbases)
+        n = self.strbases.count(base)
         if tl > 0:
-            perca = round(100.0 * na / tl, 1)
-            percc = round(100.0 * nc / tl, 1)
-            perct = round(100.0 * nt / tl, 1)
-            percg = round(100.0 * ng / tl, 1)
+            perc = round(100.0 * n / tl, 1)  # percentage with one decimal of precision
+
         else:
-            perca = 0
-            percc = 0
-            perct = 0
-            percg = 0
-
-print("Practice 1")
-
-s1=Seq("AATG")
-s1.complement()
-s1.reverse()
+            perc = 0  # if there is an empty string
+        return perc
