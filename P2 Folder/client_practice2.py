3b434d1291301b2ec35a0e01260a589008f69fff
paugarciar <paugr@yahoo.es>
2019-02-17 15:16:54 +0100

    practice 2

---
+++ P2 Folder/client_practice2.py
@@ -0,0 +1,42 @@
+# definition of the class
+class Seq:
+    """A class for representing sequences"""
+
+    def __init__(self, strbases):
+        self.strbases = strbases  # self.strbases now represents my initial string
+
+    def complement(self):
+        comp_str = ""  # this empty string will receive the complementary bases
+        pattern = {'A': 'T', 'C': 'G', 'G': 'C', 'T': 'A'}
+        for base in self.strbases:
+            comp_str += pattern[base]  # complementary bases are searched in the dictionary
+        return comp_str
+
+    def reverse(self):
+        rev_str = self.strbases[::-1]  # inverting the order of the bases
+        return rev_str
+
+
+# main program
+import socket
+
+# loop to go on chatting
+connected = True
+while connected:
+    # socket for communicating with the server
+    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+    print("socket created")
+
+    PORT = 8081
+    IP = "192.168.1.106"
+
+    s.connect((IP, PORT))
+    seq1 = Seq(input("\nwrite a sequence to obtain the reverse-complement: "))
+    seq2 = Seq(seq1.reverse()).complement()
+    print("reverse-complement sequence: ", seq2)
+    # converting the strings into binary code
+    s.send(str.encode(seq2))
+
+    s.close()
+    print("--sequence sent to the server--")
