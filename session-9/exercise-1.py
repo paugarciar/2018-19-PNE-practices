45df758d02c47f328a0471c4d855cfc666775234
paugarciar <paugr@yahoo.es>
2019-02-23 12:00:03 +0100

    Program to print client messages in a different color and closing the server when the client writes EXIT

--- session-9/exercise-1.py
+++ session-9/exercise-1.py
@@ -1,19 +1,25 @@
+# Program to print client messages in a different color
+# and closing the server when the client writes EXIT
+
 import socket
 import termcolor
 PORT = 8081
-IP = "212.128.253.73"
+IP = "192.168.1.106"
 MAX_OPEN_REQUEST = 5
+ex = [0]  # this list will be modified to close the loop
 
 
 def process_client(cs):
 
     # reading message from the client
-    msg = cs.recv(2048).decode("utf-8")  # maximum length that we want to receive
-    termcolor.cprint(msg, "yellow")
+    msg = cs.recv(2048).decode("utf-8")  # translating the message
+    termcolor.cprint(msg, "yellow")  # printing it in yellow
+
+    if msg == "EXIT":
+        ex.append(1)  # add an element that changes length to 2
 
     # sending the message back to the client (because we are an echo server)
     cs.send(str.encode(msg))
-
     cs.close()
 
 
@@ -26,7 +32,7 @@ serversocket.listen(MAX_OPEN_REQUEST)
 
 print("Socket ready: {}".format(serversocket))
 
-on =True
+on = True
 while on:
 
     print("waiting for connections at: {}, {}".format(IP, PORT))
@@ -37,5 +43,5 @@ while on:
     print("attending client: {}".format(address))
 
     process_client(clientsocket)
-    if msg == "EXIT":
-        on = False
+    if len(ex) == 2:
+        on = False  # going out of the loop
