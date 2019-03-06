2902924f6f6b4755d5e8c863fc49877ab96a72b6
paugr <paugr@yahoo.es>
2019-03-06 17:02:00 +0100

    using classes to change the page depending on the information of the request

---
+++ session-13/exercise-2.py
@@ -0,0 +1,37 @@
+import http.server
+import socketserver
+
+PORT = 8010
+
+
+class TestHandler(http.server.BaseHTTPRequestHandler):
+
+    def do_GET(self):
+        print("GET received")
+        print("Request line:"+self.requestline)
+        print(" Cmd: "+self.command)
+        print(" Path: "+self.path)
+
+        if self.path == "/":
+            page = "index.html"
+        else:
+            page = "error.html"
+        f = open(page, "r")
+        content = f.read()
+        f.close()
+
+        self.send_response(200)
+        self.send_header('Content-Type', 'text/html')
+        self.send_header('Content-Length', len(str.encode(content)))
+        self.end_headers()
+
+        self.wfile.write(str.encode(content))
+        return
+
+
+Handler = TestHandler
+
+with socketserver.TCPServer(("", PORT), Handler) as httpd:
+    print("Serving at PORT", PORT)
+
+    httpd.serve_forever()
