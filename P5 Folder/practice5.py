6a1e328c12c4aea54469ad0d75039926bea39ddb
paugr <paugr@yahoo.es>
2019-03-06 17:51:36 +0100

    Practice 5:web server of practice 4, but using the http module

---
+++ P5 Folder/practice5.py
@@ -0,0 +1,40 @@
+import http.server
+import socketserver
+
+PORT = 8001
+
+class TestHandler(http.server.BaseHTTPRequestHandler):
+
+    def do_GET(self):
+        print("GET received")
+        print("Request line:"+self.requestline)
+        print(" Cmd: "+self.command)
+        print(" Path: "+self.path)
+
+        if self.path == "/":
+            page = "index.html"
+        elif self.path == "/blue":
+            page = "blue.html"
+        elif self.path == "/pink":
+            page = "pink.html"
+        else:
+            page = "error.html"
+        f = open(page, "r")
+        content = f.read()
+        f.close()
+
+        self.send_response(200)
+        self.send_header('Content-Type', 'text/html')
+        self.send_header('Content-Length', len(str.encode(content)))
+        self.end_headers()
+
+        self.wfile.write(str.encode(content))
+        return
+
+
+Handler = TestHandler
+
+with socketserver.TCPServer(("", PORT), Handler) as httpd:
+    print("Serving at PORT", PORT)
+
+    httpd.serve_forever()
