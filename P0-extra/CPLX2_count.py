46d94731c37f883373b559c0f4773d8d268951df
paugarciar <paugr@yahoo.es>
2019-04-19 12:20:20 +0200

    Program that counts the bases of the gen CPLX2

--- P0-extra/CPLX2_count.py
+++ P0-extra/CPLX2_count.py
@@ -1,18 +1,20 @@
-print('program to count the different bases of the gen CPLX2')
-print('')
-f=open('CPLX2.txt', 'r')
-info=f.read()
-info=info.split('\n')
-A=0
-C=0
-T=0
-G=0
+# Program that counts the bases of the gen CPLX2
+
+print('Program to count the different bases of the gen CPLX2')
+print()
+f = open('CPLX2.txt', 'r')
+info = f.read()
+info = info.split('\n')
+A = 0
+C = 0
+T = 0
+G = 0
 for index in info:
-    if len(index)!=0 and index[0]!='>':
-        A+=index.count('A')
-        C+=index.count('C')
-        T+=index.count('T')
-        G+=index.count('G')
+    if len(index) != 0 and index[0] != '>':
+        A += index.count('A')
+        C += index.count('C')
+        T += index.count('T')
+        G += index.count('G')
 
 print('A:', A)
 print('C:', C)
