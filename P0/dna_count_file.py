9127be42110ea781916a0983fd8b8e20b1ffdf72
paugarciar <paugr@yahoo.es>
2019-04-19 11:28:59 +0200

    Program to count the number of bases of a DNA sequence that is contained in a text file

--- P0/dna_count_file.py
+++ P0/dna_count_file.py
@@ -1,14 +1,16 @@
-print('exercise 5, tools ii')
-print('program to count the total number of bases in a DNA sequence and the number of each different base from a text file')
+# Program to count the number of bases of a DNA sequence that is contained in a text file
 
-f=open('DNA_textfordna_count_file.txt', 'r')
-dna_seq=f.read()
+print('--exercise 5, tools ii--')
+print('Counting the total number of bases in a DNA sequence and the number of each different base from a text file')
+
+f = open('DNA_textfordna_count_file.txt', 'r')
+dna_seq = f.read()
 print('the length of the sequence is ', len(dna_seq))
 
-A=dna_seq.count('A')
-C=dna_seq.count('C')
-T=dna_seq.count('T')
-G=dna_seq.count('G')
+A = dna_seq.count('A')
+C = dna_seq.count('C')
+T = dna_seq.count('T')
+G = dna_seq.count('G')
 
 print('A:', A)
 print('C:', C)
