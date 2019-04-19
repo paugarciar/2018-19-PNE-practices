e849157f02a9ce8c1625b72295ced9e4129fdf42
paugarciar <paugr@yahoo.es>
2019-04-19 12:17:55 +0200

    Program that opens the CPLX2 gen and prints all the information contained in it

--- P0-extra/CPLX2_print.py
+++ P0-extra/CPLX2_print.py
@@ -1,6 +1,8 @@
-print('program to print the information of the gen CPLX2')
-print('')
-f=open('CPLX2.txt', 'r')
-info=f.read()
+# Program that opens the CPLX2 gen and prints all the information contained in it
+
+print('Program to print the information of the gen CPLX2')
+print()
+f = open('CPLX2.txt', 'r')
+info = f.read()
 print(info)
 f.close()
