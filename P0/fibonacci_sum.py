e3f695ab2a791ee52cb13ceadb3f7c9a280a8101
paugarciar <paugr@yahoo.es>
2019-04-19 11:17:55 +0200

    Adding the fibonacci terms from 1 to n. The n parameter should be enter by the user

--- P0/fibonacci_sum.py
+++ P0/fibonacci_sum.py
@@ -1,28 +1,30 @@
+# Program that sums the fibonacci series therms until n (parameter entered by the user)
+
 print('this program sums the fibonacci terms')
 print('attention, for a correct result n>0, (the first position is 1)')
 print('')
-p=int(input('insert the position of the term of fibonacci series (n):'))
-l=[0,1]
+p = int(input('insert the position of the term of fibonacci series (n):'))
+ls = [0, 1]
+
 
 def fibonacci(n):
-    for index in range(3,n+1):
-        sum=l[index-3]+l[index-2]
-        l.append(sum)
-    if p==1:
-        nth_term=l[0]
+    for index in range(3, n+1):
+        ls.append(ls[index-3]+ls[index-2])
+    if p == 1:
+        nth_term = ls[0]
     else:
-        nth_term= l[-1]
+        nth_term = ls[-1]
     return nth_term
 
 
 def fibonacci_sum(n):
-    value=fibonacci(n)
-    sumatory=0
-    if value!=0:
-        for i in l:
-            sumatory += i
-    return sumatory
+    value = fibonacci(n)
+    summat = 0
+    if value != 0:
+        for i in ls:
+            summat += i
+    return summat
 
 
-value2=fibonacci_sum(p)
+value2 = fibonacci_sum(p)
 print('the value of the sum of the fibonacci terms until the position', p, 'is:', value2)
