e301d7ba98527a080924c34238bd6573adf9a711
paugarciar <paugr@yahoo.es>
2019-04-22 17:43:30 +0200

    Web socket-server that gives the user access to three different html pages

--- P4 Folder/practice4.py
+++ P4 Folder/practice4.py
@@ -1,23 +1,32 @@
+# Web server that gives the user access to three different html pages
+# It has been programed using sockets
 import socket
 import termcolor
 
-# Change this IP to yours!!!!!
-IP = "212.128.253.70"
+# IP AND PORT
+IP = "192.168.1.20"
 PORT = 8081
 MAX_OPEN_REQUESTS = 5
 
 
+# Processing the request received
 def process_client(cs):
     """Process the client request.
     Parameters:  cs: socket for communicating with the client"""
 
-    # Read client message. Decode it as a string
+    # Reading and decoding the client message
     msg = cs.recv(2048).decode("utf-8")
-    lines_msg = msg.split("\n")
-    first_line = lines_msg[0].split(" ")
+
+    # Separating the message in different parts
+    lines_msg = msg.split("\n")  # Making a list of the lines of the message
+    first_line = lines_msg[0].split(" ")  # Making a list of the first line of the message
     print()
+
+    # Printing the complete request message in the server
     print("Request message: ")
     termcolor.cprint(msg, 'green')
+
+    # Accessing to html pages
     if first_line[1] == "/":
         page = "index.html"
     elif first_line[1] == "/blue":
@@ -27,20 +36,21 @@ def process_client(cs):
     else:
         page = "error.html"
     f = open(page, "r")
-    content = f.read()
+    content = f.read()  # Saving the contents in the variable content
     f.close()
-    status_line = "HTTP/1.1 200 OK\r\n"
 
+    # Generating and sending the response message according to the request
+    status_line = "HTTP/1.1 200 OK\r\n"
     header = "content-type: text/html\r\n"
     header += "content-length: {}\r\n".format(len(str.encode(content)))
-
     response_msg = status_line + header + "\r\n" + content
     cs.send(str.encode(response_msg))
-    # Close the socket
+
+    # Closing the socket
     cs.close()
 
 
-# MAIN PROGRAM
+# -- MAIN PROGRAM
 
 # create an INET, STREAMing socket
 serversocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
